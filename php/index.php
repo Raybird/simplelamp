6371f2f77cad039a9eff465594e684954a0c7e95
raybird <a691228@gmail.com>
2015-09-22 08:41:35 +0800

    =Add ignore setting

--- php/index.php
+++ php/index.php
@@ -1,13 +1,2 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	
-	
-	<script src="nodejs/node_modules/angular-builds/angular.js"></script>
-	<script src="nodejs/node_modules/angular-route/angular-route.js"></script>
-</body>
-</html>
+<?php
+phpinfo();
